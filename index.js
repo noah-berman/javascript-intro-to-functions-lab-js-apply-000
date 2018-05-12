96263025d3826b950022ce82834ccfbe7f2c4ca2
Noah Berman <noahberm@gmail.com>
2018-05-12 19:19:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function shout(string) { 
 return string.toUpperCase()
 }
+<<<<<<< HEAD
 
 function whisper(string) {
   return string.toLowerCase()
 }
+=======
+>>>>>>> 5a9a8931e926ad1397cbb0a8b3f33957bcce1048
